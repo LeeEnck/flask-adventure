acd98ac137659d9d7bd1901d9c2e5b2f8bf7f818
LeeEnck <eaglesfootball33@gmail.com>
2020-03-27 13:21:23 -0400

    Added more text into each stage description

--- sheol-adventure/stage.py
+++ sheol-adventure/stage.py
@@ -15,12 +15,12 @@ class Stage():
 @bp.route('/game/<stage_name>', methods=('GET', 'POST'))
 def stage(stage_name):
     descriptions = {
-        'hell': Stage('hell', 'This is the hell stage', 'look around, '),
-        'lavapit': Stage('lavapit', 'This is the lavapit stage', '',),
-        'graveyard': Stage('graveyard', 'This is the graveyard stage', '',),
-        'lavafalls': Stage('lavafalls', 'This is the lavafalls stage', '',),
-        'luciferslair': Stage('luciferslair','This is the luciferslair stage', '',),
-        'holygrail': Stage('holygrail', 'This is the holygrail stage', '',)
+        'hell': Stage('hell', 'This is the hell stage', 'Venture, '),
+        'lavapit': Stage('lavapit', 'This is the lavapit stage', 'Spot the Plumbus',),
+        'graveyard': Stage('graveyard', 'This is the graveyard stage', 'Explore',),
+        'lavafalls': Stage('lavafalls', 'This is the lavafalls stage', 'Build a Raft',),
+        'luciferslair': Stage('luciferslair','This is the luciferslair stage', 'Attack Lucifer',),
+        'holygrail': Stage('holygrail', 'This is the holygrail stage', 'Flip the coin',)
         }
 
     if request.method == 'POST':
