139efe2fca760c97fc68cd882a43604216e8a075
LeeEnck <eaglesfootball33@gmail.com>
2020-03-27 16:12:21 -0400

    Added more description for each stage

--- sheol-adventure/stage.py
+++ sheol-adventure/stage.py
@@ -15,12 +15,29 @@ class Stage():
 @bp.route('/game/<stage_name>', methods=('GET', 'POST'))
 def stage(stage_name):
     descriptions = {
-        'hell': Stage('hell', 'This is the hell stage', 'Venture or weep,'),
-        'lavapit': Stage('lavapit', 'This is the lavapit stage', 'Spot the Plumbus',),
-        'graveyard': Stage('graveyard', 'This is the graveyard stage', 'Explore',),
-        'lavafalls': Stage('lavafalls', 'This is the lavafalls stage', 'Build a Raft',),
-        'luciferslair': Stage('luciferslair','This is the luciferslair stage', 'Attack Lucifer',),
-        'holygrail': Stage('holygrail', 'This is the holygrail stage', 'Flip the coin',)
+        'hell': Stage('hell', """You have been sent to hell for your vulger and dispicable behavior.
+               You can venture into the hellish boundaries.
+               You can curl up into a ball and weep""", 'Venture or weep,'),
+
+        'lavapit': Stage('lavapit', """You have now arrived at the lava pit, and see a
+              giant plumbus levitating""", 'take it, leave it, punt it',),
+
+        'graveyard': Stage('graveyard', """You have approached the graveyard of the banished
+              and now you must choose what to do next. Either ignore
+              the graveyard and continue on, or go into the undead land.""", 'Explore',),
+
+        'lavafalls': Stage('lavafalls', """You feel the unwrenching heat of the lavafalls ahead and now have to overcome
+               the largest obstacle that you have yet to discover. """, 'Build a Raft or alternate route',),
+
+        'luciferslair': Stage('luciferslair', """You fought through much of what Hell has to offer but now you
+              must approach the demonic entity himself. While feeling his presence
+              since you have arrived you feel a cold touch to your shoulder.""", 'Attack Lucifer hand to hand combat or throw holy water',),
+
+        'holygrail': Stage('holygrail', """You spoken his forsaken name and have also flipped the coin in the air.
+              At first nothing happens, then all of a sudden you start falling into what seems like an endless black hole. Then
+              you awake sweaty and cold in a hospital bed, free of the shackles of Hell.
+              Congrats on proving your stoic perseverance as an individual especially
+              against the cruel unwrenching being Lucifer!""", 'Flip the coin',)
         }
 
     if request.method == 'POST':
